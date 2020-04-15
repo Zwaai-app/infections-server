11f10396c5a2cc09c8e806c980b889cc3763974e
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-04-15 17:57:11 +0200

    Adds prettier

--- spaces-frontend/src/App.tsx
+++ spaces-frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
