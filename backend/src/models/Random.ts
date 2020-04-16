300c88978c2fdcd291f004e16ce4e63d984bc946
Mark Spanbroek <mark@spanbroek.net>
2020-04-16 10:17:43 +0200

    Store submitted randoms in mongodb

---
+++ backend/src/models/Random.ts
@@ -0,0 +1,6 @@
+import {Schema, Document, model} from "mongoose";
+
+export type RandomDocument = Document & { value: string }
+
+const schema = new Schema({ value: String });
+export const Random = model<RandomDocument>("Random", schema);
--- backend/test/randoms.test.ts
+++ backend/test/randoms.test.ts
@@ -1,5 +1,12 @@
 import request from "supertest";
+import { Random } from "../src/models/Random";
 import app from "../src/app";
+
+afterEach(async () => {
+  if (await Random.exists({})) {
+    await Random.collection.drop();
+  }
+});
 
 it("returns an empty list of infected randoms", async () => {
   await request(app)
@@ -24,4 +31,16 @@ it("rejects body that is not json", async () => {
     .set("Content-Type", "text/plain")
     .send("")
     .expect(415);
+});
+
+it("adds submitted randoms to list of infected randoms", async () => {
+  const randoms = ["one", "two"];
+  await request(app)
+    .post("/infected-randoms/submit")
+    .set("Content-Type", "application/json")
+    .send(randoms);
+  await request(app)
+    .get("/infected-randoms")
+    .expect("Content-Type", /json/)
+    .expect(200, { randoms });  
 });
