419b4f47f300722cfff446e2ae4a5cf512661e69
Mark Spanbroek <mark@spanbroek.net>
2020-04-16 10:17:43 +0200

    Store submitted randoms in mongodb

--- backend/src/controllers/randoms.ts
+++ backend/src/controllers/randoms.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
-export const getInfectedRandoms = (request: Request, response: Response)  => {
-  response.json({ randoms: [] });
+import { Random } from "../models/Random";
+
+export const getInfectedRandoms = async (request: Request, response: Response)  => {
+  const values: string[] = [];
+  for (const random of await Random.find({})) {
+    values.push(random.value);
+  }
+  response.json({ randoms: values });
 };
 
-export const addInfectedRandoms = (request: Request, response: Response) => {
+export const addInfectedRandoms = async (request: Request, response: Response) => {
   if (!request.is("application/json")) {
     response.send(415);
     return;
-  } 
-  response.send();
+  }
+  
+  for (const value of request.body) {
+    const random = new Random();
+    random.value = value;
+    await random.save();          
+  }; 
+
+  response.sendStatus(200);
 };
