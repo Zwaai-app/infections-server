602f646f83d299480040233af652fa81cfbc22d2
Mark Spanbroek <mark@spanbroek.net>
2020-04-16 10:16:18 +0200

    Formatting

--- backend/test/randoms.test.ts
+++ backend/test/randoms.test.ts
@@ -3,11 +3,11 @@ import app from "../src/app";
 
 it("returns an empty list of infected randoms", async () => {
   await request(app)
-          .get("/infected-randoms")
-          .expect("Content-Type", /json/)
-          .expect(200, {
-            randoms: []
-          });
+    .get("/infected-randoms")
+    .expect("Content-Type", /json/)
+    .expect(200, {
+      randoms: []
+    });
 });
 
 it("accepts a list of infected randoms", async () => {
