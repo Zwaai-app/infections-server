54d692a8bdfbc8441d1b4e59c669d65f1f25399d
Mark Spanbroek <mark@spanbroek.net>
2020-04-16 08:51:14 +0200

    Accept submitted list of infected randoms

--- backend/src/controllers/randoms.ts
+++ backend/src/controllers/randoms.ts
@@ -2,3 +2,11 @@ import { Request, Response } from "express";
 export const getInfectedRandoms = (request: Request, response: Response)  => {
   response.json({ randoms: [] });
 };
+
+export const addInfectedRandoms = (request: Request, response: Response) => {
+  if (!request.is("application/json")) {
+    response.send(415);
+    return;
+  } 
+  response.send();
+};
