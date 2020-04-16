f1265901ac56eba374b5e9edd0b464fe28bf0093
Mark Spanbroek <mark@spanbroek.net>
2020-04-16 15:24:45 +0200

    Do not output debug logging during tests

--- backend/src/util/logger.ts
+++ backend/src/util/logger.ts
@@ -1,10 +1,17 @@
 import winston from "winston";
 
+let consoleLevel: string;
+if (process.env.NODE_ENV === "production") {
+  consoleLevel = "error";
+} else if (process.env.NODE_ENV === "test") {
+  consoleLevel = "error";
+} else {
+  consoleLevel = "debug";
+}
+
 const options: winston.LoggerOptions = {
     transports: [
-        new winston.transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
+        new winston.transports.Console({ level: consoleLevel }),
         new winston.transports.File({ filename: "debug.log", level: "debug" })
     ]
 };
