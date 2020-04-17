6f4cf5904a9cc42a378f37062af9b322a171f1e4
Stefan van den Oord <soord@mac.com>
2020-04-17 11:28:34 +0200

    Removes i18n again

--- spaces-frontend/src/index.tsx
+++ spaces-frontend/src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import i18n from './i18n'
-
-// tslint:disable-next-line: no-floating-promises
-i18n.changeLanguage('nl-NL')
 
 ReactDOM.render(
   <React.StrictMode>
