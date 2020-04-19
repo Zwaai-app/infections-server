1655afd00a25ffa86baad9b79e671c075e66e4ea
Stefan van den Oord <soord@mac.com>
2020-04-19 08:52:21 +0200

    Translates password requirements

--- spaces-frontend/src/Register/DataEntry.tsx
+++ spaces-frontend/src/Register/DataEntry.tsx
@@ -23,17 +23,32 @@ const tPasswordsDiffer = t(
   'Wachtwoorden zijn niet hetzelfde'
 )
 
+const tPasswordLength = t(
+  'registration.passwordMinimalLength',
+  'tenminste 8 karakters'
+)
+
+const tPasswordOneCapital = t(
+  'registration.passwordOneCapital',
+  'tenminste een hoofdletter'
+)
+
+const tPasswordOneNumber = t(
+  'registration.passwordOneNumber',
+  'tenminste een cijfer'
+)
+
 const equalPasswords = (p1: string, p2: string): Either<string, string> =>
   p1 === p2 ? right(p1) : left(tPasswordsDiffer)
 
 const minLength = (s: string): Either<string, string> =>
-  s.length >= 8 ? right(s) : left('at least 8 characters')
+  s.length >= 8 ? right(s) : left(tPasswordLength)
 
 const oneCapital = (s: string): Either<string, string> =>
-  /[A-Z]/g.test(s) ? right(s) : left('at least one capital letter')
+  /[A-Z]/g.test(s) ? right(s) : left(tPasswordOneCapital)
 
 const oneNumber = (s: string): Either<string, string> =>
-  /[0-9]/g.test(s) ? right(s) : left('at least one number')
+  /[0-9]/g.test(s) ? right(s) : left(tPasswordOneNumber)
 
 const minLengthV = lift(minLength)
 const oneCapitalV = lift(oneCapital)
