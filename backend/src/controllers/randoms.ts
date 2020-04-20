41bde39a9f40a9a88a3f2b2896533d46c3d5b364
Stefan van den Oord <soord@mac.com>
2020-04-20 12:03:29 +0200

    Formats using prettier

--- backend/src/controllers/randoms.ts
+++ backend/src/controllers/randoms.ts
@@ -1,25 +1,31 @@
-import { Request, Response } from "express";
-import { Random } from "../models/Random";
+import { Request, Response } from 'express'
+import { Random } from '../models/Random'
 
-export const getInfectedRandoms = async (request: Request, response: Response)  => {
-  const values: string[] = [];
+export const getInfectedRandoms = async (
+  request: Request,
+  response: Response
+) => {
+  const values: string[] = []
   for (const random of await Random.find({})) {
-    values.push(random.value);
+    values.push(random.value)
   }
-  response.json({ randoms: values });
-};
+  response.json({ randoms: values })
+}
 
-export const addInfectedRandoms = async (request: Request, response: Response) => {
-  if (!request.is("application/json")) {
-    response.sendStatus(415);
-    return;
+export const addInfectedRandoms = async (
+  request: Request,
+  response: Response
+) => {
+  if (!request.is('application/json')) {
+    response.sendStatus(415)
+    return
   }
-  
+
   for (const value of request.body) {
-    const random = new Random();
-    random.value = value;
-    await random.save();          
-  };
+    const random = new Random()
+    random.value = value
+    await random.save()
+  }
 
-  response.sendStatus(200);
-};
+  response.sendStatus(200)
+}
