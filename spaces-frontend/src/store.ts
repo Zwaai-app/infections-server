c96601b13ef3da1c170fb1c2e4d883ea2cc2aa57
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-04-21 14:51:36 +0200

    Enables signing up with server

--- spaces-frontend/src/store.ts
+++ spaces-frontend/src/store.ts
@@ -5,11 +5,31 @@ import {
 } from '@reduxjs/toolkit'
 import rootReducer, { RootState } from './rootReducer'
 import logger from 'redux-logger'
+import { createEpicMiddleware, combineEpics } from 'redux-observable'
+import { ActionType } from 'typesafe-actions'
+import { RegisterUser } from './Register/epic'
+
+type SystemActionsWithPayload = RegisterUser.Actions
+type SystemActions = ActionType<SystemActionsWithPayload>
+type finalActions = SystemActions
+
+const epicMiddleware = createEpicMiddleware<
+  finalActions,
+  finalActions,
+  RootState
+>()
+const rootEpic = combineEpics(RegisterUser.epic)
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [...getDefaultMiddleware<RootState>(), logger as Middleware],
+  middleware: [
+    epicMiddleware,
+    ...getDefaultMiddleware<RootState>(),
+    logger as Middleware,
+  ],
 })
+
+epicMiddleware.run(rootEpic)
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept('./rootReducer', () => {
