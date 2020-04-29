1a574b3dac240a9098ef2a5b328a7aa4678a212e
Stefan van den Oord <soord@mac.com>
2020-04-29 16:49:13 +0200

    Adds test for error case

--- spaces-frontend/src/Register/epic.test.ts
+++ spaces-frontend/src/Register/epic.test.ts
@@ -1,14 +1,18 @@
 import { epic, PostSignupFn } from './epic'
 import { TestScheduler } from 'rxjs/testing'
-import { setRegistrationData, signupSucceeded } from './registerSlice'
+import {
+  setRegistrationData,
+  signupSucceeded,
+  signupFailed
+} from './registerSlice'
 import { RootState } from '../rootReducer'
 import { StateObservable } from 'redux-observable'
 import store from '../store'
-import { Subject } from 'rxjs'
+import { Subject, throwError } from 'rxjs'
 
-const testScheduler = new TestScheduler((actual, expected) => {
+const deepEqual = (actual: any, expected: any) => {
   expect(actual).toEqual(expected)
-})
+}
 
 const initialStateObservable = () =>
   new StateObservable<RootState>(new Subject(), store.getState())
@@ -21,14 +25,44 @@ const validRegistrationDataAction = setRegistrationData({
 })
 
 describe('Register epic', () => {
+  let testScheduler: TestScheduler
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler(deepEqual)
+  })
+
   test('happy path', () => {
     const actionToTest = validRegistrationDataAction
+    const response = 'some response'
     testScheduler.run(({ hot, cold, expectObservable }) => {
       const state$ = initialStateObservable()
-      const action$ = hot('-a', { a: actionToTest }) as any
-      const postSignupSpy: PostSignupFn = () => cold('--a', { a: 'response' })
+      const action$ = hot('-i', { i: actionToTest }) as any
+      const postSignupSpy: PostSignupFn = () => cold('--r', { r: { response } })
       const output$ = epic(action$, state$, { postSignupFn: postSignupSpy })
-      expectObservable(output$).toBe('---a', { a: signupSucceeded('response') })
+      expectObservable(output$).toBe('---o', { o: signupSucceeded(response) })
+    })
+  })
+
+  test('server returns error', () => {
+    const errors = [{ value: 'bar', msg: 'Email is not valid', param: 'email' }]
+    const error = new MockError('test error', errors)
+    testScheduler.run(({ hot, expectObservable }) => {
+      const postSignup = () => throwError(error)
+      const state$ = initialStateObservable()
+      const action$ = hot('-i', { i: validRegistrationDataAction }) as any
+      const output$ = epic(action$, state$, { postSignupFn: postSignup })
+      expectObservable(output$).toBe('-o', {
+        o: signupFailed({ message: error.message, errors })
+      })
     })
   })
 })
+
+class MockError extends Error {
+  response: any
+
+  constructor (message: string, errors: any[]) {
+    super(message)
+    this.response = { errors }
+  }
+}
--- spaces-frontend/src/Register/epic.ts
+++ spaces-frontend/src/Register/epic.ts
@@ -1,5 +1,5 @@
 import { map, flatMap, catchError } from 'rxjs/operators'
-import { ajax, AjaxError } from 'rxjs/ajax'
+import { ajax, AjaxError, AjaxResponse } from 'rxjs/ajax'
 import { of, Observable } from 'rxjs'
 import { Epic, ofType } from 'redux-observable'
 import { ActionType } from 'typesafe-actions'
@@ -23,33 +23,37 @@ export type PostSignupFn = (
   action: SetRegistrationDataAction
 ) => Observable<any>
 
-const postSignup: PostSignupFn = action => {
-  return ajax({
+const postSignup: PostSignupFn = action =>
+  ajax({
     url: 'http://localhost:3000/api/v1/signup',
     method: 'POST',
     crossDomain: true,
+    responseType: 'json',
     body: {
       email: action.payload.email,
       password: action.payload.password,
       confirmPassword: action.payload.password
     }
-  }).pipe(map(r => r.response))
-}
+  })
 
-const success = (response: any) => signupSucceeded(response)
+const success = (r: AjaxResponse) => signupSucceeded(r.response)
 
-const failed = (error: AjaxError) =>
+const failed = (e: AjaxError) =>
   of(
     signupFailed({
-      message: error.message,
-      errors: error.response.errors
+      message: e.message,
+      errors: e.response?.errors || []
     })
   )
+
+interface EpicDeps {
+  postSignupFn?: PostSignupFn
+}
 
 export const epic: Epic<Actions, Actions, RootState> = (
   action$,
   _state$,
-  { postSignupFn = postSignup }: { postSignupFn: PostSignupFn }
+  { postSignupFn = postSignup }: EpicDeps = {}
 ) =>
   action$.pipe(
     ofType<Actions, SetRegistrationDataAction>(setRegistrationData.type),
