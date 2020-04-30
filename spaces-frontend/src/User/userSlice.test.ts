b500b8c0d2f0a6349f14493ef4b2a9fe9c7772af
Stefan van den Oord <soord@mac.com>
2020-04-30 18:19:11 +0200

    Adds loginSucceeded and loginFailed actions

--- spaces-frontend/src/User/userSlice.test.ts
+++ spaces-frontend/src/User/userSlice.test.ts
@@ -1,4 +1,21 @@
-import reducer, { UserState, logout } from './userSlice'
+import reducer, {
+  UserState,
+  logout,
+  loginSucceeded,
+  loginFailed
+} from './userSlice'
+
+it('stores the token when login succeeds', () => {
+  const state: UserState = { token: null }
+  const newState = reducer(state, loginSucceeded('some token'))
+  expect(newState.token).toBe('some token')
+})
+
+it('clears the token on login error', () => {
+  const state: UserState = { token: 'some token' }
+  const newState = reducer(state, loginFailed(new Error()))
+  expect(newState.token).toBeNull()
+})
 
 it('clears token on logout', () => {
   const state: UserState = { token: 'some token' }
--- spaces-frontend/src/User/userSlice.ts
+++ spaces-frontend/src/User/userSlice.ts
@@ -12,12 +12,18 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    loginSucceeded (state, action: PayloadAction<string>) {
+      state.token = action.payload
+    },
+    loginFailed (state, action: PayloadAction<Error>) {
+      state.token = null
+    },
     logout (state, action: PayloadAction<void>) {
       state.token = null
     }
   }
 })
 
-export const { logout } = userSlice.actions
+export const { loginSucceeded, loginFailed, logout } = userSlice.actions
 
 export default userSlice.reducer
