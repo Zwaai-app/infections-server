093f77f9edbea8d3cf8b26e612bbbe56916d6a48
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-01 14:17:36 +0200

    Adds separate status for login failure

--- spaces-frontend/src/User/userSlice.test.ts
+++ spaces-frontend/src/User/userSlice.test.ts
@@ -11,23 +11,24 @@ const credentials = { username: 'foo', password: 'bar' }
 it('clears loggedIn when starting new login', () => {
   const state: UserState = { status: 'loggedIn' }
   const newState = reducer(state, login(credentials))
-  expect(newState.status).toBe('loggedOut')
+  expect(newState.status).toEqual('loggedOut')
 })
 
 it('sets loggedIn when login succeeds', () => {
   const state: UserState = { status: 'loggedOut' }
   const newState = reducer(state, loginSucceeded())
-  expect(newState.status).toBe('loggedIn')
+  expect(newState.status).toEqual('loggedIn')
 })
 
 it('clears loggedIn on login error', () => {
   const state: UserState = { status: 'loggedIn' }
-  const newState = reducer(state, loginFailed('some error message'))
-  expect(newState.status).toBe('loggedOut')
+  const errorMessage = 'some error message'
+  const newState = reducer(state, loginFailed(errorMessage))
+  expect(newState.status).toEqual({ error: 'some error message' })
 })
 
 it('clears loggedIn on logout', () => {
   const state: UserState = { status: 'loggedIn' }
   const newState = reducer(state, logout())
-  expect(newState.status).toBe('loggedOut')
+  expect(newState.status).toEqual('loggedOut')
 })
