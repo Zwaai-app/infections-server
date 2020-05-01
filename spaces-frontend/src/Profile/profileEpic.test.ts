71d5cfc3254d8a1640e3253bb2ecc4b16b87f8e0
Stefan van den Oord <soord@mac.com>
2020-05-01 16:08:04 +0200

    Adds profile epic

---
+++ spaces-frontend/src/Profile/profileEpic.test.ts
@@ -0,0 +1,35 @@
+import { loadProfileEpic } from './profileEpic'
+import { ActionsObservable, StateObservable } from 'redux-observable'
+import { loadProfile, profileLoaded, profileLoadFailed } from './profileSlice'
+import { RootState } from '../rootReducer'
+import { Subject, of, throwError } from 'rxjs'
+import store from '../store'
+import { initialStateObservable } from '../testUtils/stateObservable'
+import { MockAjaxError } from '../testUtils/MockAjaxError'
+
+const orgData = {
+  organizationName: 'my org',
+  organizationUrl: 'http://example.com'
+}
+
+it('can load a profile', done => {
+  expect.assertions(1)
+  const action$ = ActionsObservable.of(loadProfile())
+  const state$ = initialStateObservable()
+  const getProfileFn = () => of({ response: orgData })
+  loadProfileEpic(action$, state$, { getProfileFn }).subscribe(action => {
+    expect(action).toEqual(profileLoaded(orgData))
+    done()
+  })
+})
+
+it('reports load failures', done => {
+  expect.assertions(1)
+  const action$ = ActionsObservable.of(loadProfile())
+  const state$ = initialStateObservable()
+  const getProfileFn = () => throwError(new MockAjaxError('some error'))
+  loadProfileEpic(action$, state$, { getProfileFn }).subscribe(action => {
+    expect(action).toEqual(profileLoadFailed('some error'))
+    done()
+  })
+})
