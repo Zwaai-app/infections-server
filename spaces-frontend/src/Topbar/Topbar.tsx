f8561135f2bd50c41720cc4e3005555c07eb9142
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-01 16:28:14 +0200

    Introduces dropdown for user-related actions

--- spaces-frontend/src/Topbar/Topbar.tsx
+++ spaces-frontend/src/Topbar/Topbar.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Menu, Icon } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Menu, Icon, Dropdown } from 'semantic-ui-react'
+import { Link, useHistory } from 'react-router-dom'
 import { t } from '../i18n'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../rootReducer'
 import { logout } from '../User/userSlice'
 
 export function Topbar() {
+    const history = useHistory()
     const loggedIn = useSelector((state: RootState) => state.user.status === 'loggedIn')
     const dispatch = useDispatch()
+    const profileData = useSelector((state: RootState) => state.profile.data)
+    const organizationName = profileData?.organizationName || ''
 
     return (<Menu id='topbar' fixed='top' inverted>
         <Menu.Item>
@@ -16,7 +19,17 @@ export function Topbar() {
         </Menu.Item>
         <Menu.Menu position='right'>
             {loggedIn
-                ? <Menu.Item><Link to="/" onClick={() => dispatch(logout())}>{t('topbar.logout', 'Uitloggen')}</Link></Menu.Item>
+                ? <>
+                    <Dropdown item text={organizationName} loading>
+                        <Dropdown.Menu>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={() => {
+                                dispatch(logout())
+                                history.replace('/')
+                            }}>{t('topbar.logout', 'Uitloggen')}</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </>
                 : <>
                     <Menu.Item><Link to='/login'>{t('topbar.login', 'Inloggen')}</Link></Menu.Item>
                     <Menu.Item><Link to='/register'>{t('topbar.register', 'Registreren')}</Link></Menu.Item>
