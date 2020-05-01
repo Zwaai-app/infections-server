18d1fa118d88aafa8991826a4e5b6c3fff81c0e8
Stefan van den Oord <soord@mac.com>
2020-05-01 08:01:15 +0200

    Does not show login link when logged in

--- spaces-frontend/src/Topbar/Topbar.test.tsx
+++ spaces-frontend/src/Topbar/Topbar.test.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { Topbar } from './Topbar'
 import { MemoryRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import rootReducer from '../rootReducer'
+import { Provider } from 'react-redux'
+
+const loggedOutStore = createStore(rootReducer, { user: { token: null } })
+const loggedInStore = createStore(rootReducer, { user: { token: 'some tomen' } })
 
 it('renders login link when not logged in', () => {
-    const { getByText } = render(<MemoryRouter><Topbar /></MemoryRouter>)
+    const { getByText } = render(<Provider store={loggedOutStore}>
+        <MemoryRouter><Topbar /></MemoryRouter>
+    </Provider>)
     expect(getByText(/topbar\.login/i)).toBeInTheDocument()
+})
+
+it('renders no login link when already logged in', () => {
+    const { queryByText } = render(<Provider store={loggedInStore}>
+        <MemoryRouter><Topbar /></MemoryRouter>
+    </Provider>)
+    expect(queryByText(/topbar\.login/i)).not.toBeInTheDocument()
 })
