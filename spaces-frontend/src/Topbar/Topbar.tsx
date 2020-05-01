9d3e26d6283b75d0ee5f4921f29b49ffa3c5db96
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-01 08:01:15 +0200

    Does not show login link when logged in

--- spaces-frontend/src/Topbar/Topbar.tsx
+++ spaces-frontend/src/Topbar/Topbar.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { t } from '../i18n'
+import { useSelector } from 'react-redux'
+import { RootState } from '../rootReducer'
 
-export const Topbar = () => <Menu id='topbar' fixed='top' inverted>
-    <Menu.Item>
-        <Link to='/'>{t('topbar.home', 'Home')}</Link>
-    </Menu.Item>
-    <Menu.Menu position='right'>
-        <Menu.Item>{t('topbar.login', 'Inloggen')}</Menu.Item>
-        <Menu.Item>{t('topbar.register', 'Registreren')}</Menu.Item>
-    </Menu.Menu>
-</Menu>
+export function Topbar() {
+    const token = useSelector((state: RootState) => state.user.token)
+
+    return (<Menu id='topbar' fixed='top' inverted>
+        <Menu.Item>
+            <Link to='/'>{t('topbar.home', 'Home')}</Link>
+        </Menu.Item>
+        <Menu.Menu position='right'>
+            {token
+                ? <Menu.Item>{token}</Menu.Item>
+                : <Menu.Item>{t('topbar.login', 'Inloggen')}</Menu.Item>
+            }
+            <Menu.Item>{t('topbar.register', 'Registreren')}</Menu.Item>
+        </Menu.Menu>
+    </Menu>)
+}
