d91319e9d4cf636d59d5be43a726ae8c6ca96123
Stefan van den Oord <soord@mac.com>
2020-05-01 11:14:37 +0200

    Adds ability to logout

--- spaces-frontend/src/Topbar/Topbar.tsx
+++ spaces-frontend/src/Topbar/Topbar.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { t } from '../i18n'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../rootReducer'
+import { logout } from '../User/userSlice'
 
 export function Topbar() {
     const loggedIn = useSelector((state: RootState) => state.user.loggedIn)
+    const dispatch = useDispatch()
 
     return (<Menu id='topbar' fixed='top' inverted>
         <Menu.Item>
@@ -14,7 +16,7 @@ export function Topbar() {
         </Menu.Item>
         <Menu.Menu position='right'>
             {loggedIn
-                ? <Menu.Item>{t('topbar.logout', 'Uitloggen')}</Menu.Item>
+                ? <Menu.Item><Link to="/" onClick={() => dispatch(logout())}>{t('topbar.logout', 'Uitloggen')}</Link></Menu.Item>
                 : <>
                     <Menu.Item><Link to='/login'>{t('topbar.login', 'Inloggen')}</Link></Menu.Item>
                     <Menu.Item><Link to='/register'>{t('topbar.register', 'Registreren')}</Link></Menu.Item>
