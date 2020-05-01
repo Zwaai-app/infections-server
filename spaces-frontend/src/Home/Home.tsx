2a24afd63ae40c7c4289fa70f2e3ca2c28ff6ed7
Stefan van den Oord <soord@mac.com>
2020-05-01 18:12:40 +0200

    Adds warning about incomplete profile

--- spaces-frontend/src/Home/Home.tsx
+++ spaces-frontend/src/Home/Home.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import logo from './logo.png'
 import './Home.css'
 import { Image } from 'semantic-ui-react'
+import { CheckProfile } from '../Profile/CheckProfile'
 
 export const Home = () => {
   return (
     <div className="Home">
+      <CheckProfile />
       <header className="Home-header">
         <Image src={logo} className="Home-logo" alt="logo" />
       </header>
