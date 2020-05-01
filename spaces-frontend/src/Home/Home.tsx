3db4ae2494591d9e74485e2a869b0fc5b4af8cce
Stefan van den Oord <soord@mac.com>
2020-05-01 09:20:35 +0200

    Tweaks size of logo on home page

--- spaces-frontend/src/Home/Home.tsx
+++ spaces-frontend/src/Home/Home.tsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import logo from './logo.png'
 import './Home.css'
+import { Image } from 'semantic-ui-react'
 
 export const Home = () => {
   return (
     <div className="Home">
       <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
+        <Image src={logo} className="Home-logo" alt="logo" />
       </header>
     </div>
   )
