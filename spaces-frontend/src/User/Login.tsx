9536728d353ce3064d4bfc2d64cb4f848475686a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-01 09:31:53 +0200

    Adds logic to login form

--- spaces-frontend/src/User/Login.tsx
+++ spaces-frontend/src/User/Login.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button, Input } from 'semantic-ui-react'
 import { t } from '../i18n'
+import { useDispatch } from 'react-redux'
+import { login } from './userSlice'
 
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+
   return <div id='Login'>
     <h1>{t('login.header', 'Inloggen')}</h1>
     <Form>
@@ -11,12 +17,16 @@ export const Login = () => {
         <Input
           label='@'
           name='email'
+          value={email}
+          onChange={(_, { value }) => setEmail(value)}
           autoComplete='username'
           placeholder={t('login.usernamePlaceholder', 'e-mailadres')} />
       </Form.Field>
       <Form.Input
         label={t('login.passwordLabel', 'Wachtwoord')}
         name='password'
+        value={password}
+        onChange={(_, { value }) => setPassword(value)}
         autoComplete='password'
         type='password'
         placeholder={t('login.passwordPlaceholder', 'wachtwoord')} />
@@ -24,7 +34,8 @@ export const Login = () => {
         <Button
           className="right floated"
           primary
-          disabled
+          disabled={!email || !password}
+          onClick={() => dispatch(login({ username: email, password }))}
         >{t('login.loginButton', 'Inloggen')}</Button>
       </Form.Field>
     </Form>
