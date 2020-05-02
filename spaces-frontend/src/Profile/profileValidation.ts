a9a54a3c7c9c30381f2daaa07be1d879703a7bcc
Stefan van den Oord <soord@mac.com>
2020-05-02 23:39:32 +0200

    Defines some functions for profile validation

---
+++ spaces-frontend/src/Profile/profileValidation.ts
@@ -0,0 +1,29 @@
+import * as E from 'fp-ts/lib/Either'
+import * as O from 'fp-ts/lib/Option'
+import { parseURL, URLRecord } from 'whatwg-url'
+import { t } from '../i18n'
+import { parsePhoneNumberFromString, PhoneNumber } from 'libphonenumber-js'
+import { constant, flow } from 'fp-ts/lib/function'
+
+const tInvalidOrgName = t(
+  'profile.invalidOrganizationName',
+  'Organisatienaam moet minstens twee letters bevatten'
+)
+const tInvalidUrl = t('profile.invalidUrl', 'Ongeldige URL')
+const tInvalidPhone = t('profile.invalidPhone', 'Ongeldig telefoonnummer')
+
+export const validOrganizationName = (name: string): E.Either<string, string> =>
+  E.fromPredicate((n: string) => n.length >= 2, constant(tInvalidOrgName))(name)
+
+const validScheme = (url: URLRecord): boolean =>
+  !!url && (url.scheme === 'http' || url.scheme === 'https')
+
+export const validUrl: (url: string) => E.Either<string, URLRecord> = flow(
+  parseURL,
+  O.fromNullable,
+  O.filter(validScheme),
+  E.fromOption(constant(tInvalidUrl))
+)
+
+export const validPhone = (phone: string): E.Either<string, PhoneNumber> =>
+  E.fromNullable(tInvalidPhone)(parsePhoneNumberFromString(phone, 'NL'))
