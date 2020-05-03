7d9013670d301fdb873711522b783496e074adf7
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-03 14:51:40 +0200

    Adds a bit of documentation about i18n

--- spaces-frontend/src/i18n.ts
+++ spaces-frontend/src/i18n.ts
@@ -1,3 +1,20 @@
+/**
+ * Simple i18n function, intended to be replaced with something more advanced
+ * later. But this way at least there is single-point-of-definition for all
+ * translatable strings.
+ *
+ * This function supports template strings. For example, you can have a
+ * translation: "maximum 100KB", but you want to fill in the number from
+ * somewhere else. You can then invoke like this:
+ *
+ * ```typescript
+ * t('domain.key', 'maximum {{size}}KB', new Map([['size', `${maxSize}`]]))
+ * ```
+ *
+ * @param key key to lookup translation in translations database
+ * @param fallback value to be returned when key not found in translation database
+ * @param replacements map of template strings in translations
+ */
 export const t = function (
   key: string,
   fallback: string,
