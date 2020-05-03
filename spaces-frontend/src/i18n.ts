44efd9289c9c64c559452dc7bc81da51c7e8d539
Stefan van den Oord <soord@mac.com>
2020-05-03 14:45:14 +0200

    Allow replacement of values in translations

--- spaces-frontend/src/i18n.ts
+++ spaces-frontend/src/i18n.ts
@@ -1,3 +1,17 @@
-export const t = function (key: string, fallback: string): string {
-  return process.env.NODE_ENV === 'test' ? key : fallback
+export const t = function (
+  key: string,
+  fallback: string,
+  replacements?: Map<string, string>
+): string {
+  return process.env.NODE_ENV === 'test'
+    ? key
+    : (() => {
+        let translation = fallback
+        if (replacements) {
+          replacements.forEach((v, k) => {
+            translation = translation.replace(new RegExp(`{{${k}}}`, 'g'), v)
+          })
+        }
+        return translation
+      })()
 }
