9d9867a8b12b82e14a12c04db9db04d4ad9b3364
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-05 22:33:23 +0200

    Clears redux state on logout

--- spaces-frontend/src/rootReducer.ts
+++ spaces-frontend/src/rootReducer.ts
@@ -1,14 +1,23 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import registerReducer from './Register/registerSlice'
-import userReducer from './User/userSlice'
+import userReducer, { logout } from './User/userSlice'
 import profileReducer from './Profile/profileSlice'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   register: registerReducer,
   user: userReducer,
   profile: profileReducer
 })
+export type RootState = ReturnType<typeof appReducer>
 
-export type RootState = ReturnType<typeof rootReducer>
+const initialState = appReducer(undefined, { type: 'GET_INITIAL_STATE' })
+
+type RootReducerType = typeof appReducer
+const rootReducer: RootReducerType = (state, action) => {
+  if (action.type === logout.type) {
+    state = initialState
+  }
+  return appReducer(state, action)
+}
 
 export default rootReducer
