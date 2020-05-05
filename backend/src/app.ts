f14448f81a17ae6b5665f9f526526d0331f56d4e
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-05 14:13:04 +0200

    Increases upload limit to allow uploading logo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -62,8 +62,8 @@ app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
 app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }))
 app.use(
   session({
     resave: true,
