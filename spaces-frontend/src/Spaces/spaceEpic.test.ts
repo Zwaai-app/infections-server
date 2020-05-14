47c32f353e4eb36884d09851bb409b0c89ef3715
Stefan van den Oord <soord@mac.com>
2020-05-14 17:39:36 +0200

    Improves tests

--- spaces-frontend/src/Spaces/spaceEpic.test.ts
+++ spaces-frontend/src/Spaces/spaceEpic.test.ts
@@ -13,8 +13,8 @@ import * as O from 'fp-ts/lib/Option'
 import { initialStateObservable } from '../testUtils/stateObservable'
 import { storeNewSpaceEpic, loadSpacesEpic } from './spaceEpic'
 import { of, throwError } from 'rxjs'
-import { Action } from '@reduxjs/toolkit'
 import { MockAjaxError } from '../testUtils/MockAjaxError'
+import { toArray } from 'rxjs/operators'
 
 const spaceData = {
   name: 'foo',
@@ -23,34 +23,35 @@ const spaceData = {
 }
 
 it('can create a new space', done => {
-  expect.assertions(2)
   const action$ = ActionsObservable.of(createSpace(spaceData))
   const state$ = initialStateObservable()
   const storeNewSpaceFn = () => of({})
-  let dispatchedActions: Action[] = []
-  storeNewSpaceEpic(action$, state$, { storeNewSpaceFn }).subscribe(action => {
-    dispatchedActions.push(action)
-    if (dispatchedActions.length === 2) {
-      expect(dispatchedActions[0]).toEqual(storeNewSpaceStarted())
-      expect(dispatchedActions[1]).toEqual(storeNewSpaceSucceeded())
+  storeNewSpaceEpic(action$, state$, { storeNewSpaceFn })
+    .pipe(toArray())
+    .subscribe(emittedActions => {
+      expect(emittedActions).toEqual([
+        storeNewSpaceStarted(),
+        storeNewSpaceSucceeded(),
+        loadSpaces()
+      ])
       done()
-    }
-  })
+    })
 })
 
 it('reports store errors', done => {
   const action$ = ActionsObservable.of(createSpace(spaceData))
   const state$ = initialStateObservable()
   const storeNewSpaceFn = () => throwError(new MockAjaxError('some error'))
-  let dispatchedActions: Action[] = []
-  storeNewSpaceEpic(action$, state$, { storeNewSpaceFn }).subscribe(action => {
-    dispatchedActions.push(action)
-    if (dispatchedActions.length === 2) {
-      expect(dispatchedActions[0]).toEqual(storeNewSpaceStarted())
-      expect(dispatchedActions[1]).toEqual(storeNewSpaceFailed('some error'))
+  storeNewSpaceEpic(action$, state$, { storeNewSpaceFn })
+    .pipe(toArray())
+    .subscribe(emittedActions => {
+      expect(emittedActions).toEqual([
+        storeNewSpaceStarted(),
+        storeNewSpaceFailed('some error'),
+        loadSpaces()
+      ])
       done()
-    }
-  })
+    })
 })
 
 it('can load spaces', () => {
