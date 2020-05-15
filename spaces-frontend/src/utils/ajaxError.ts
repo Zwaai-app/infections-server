f207d12e2696c1b350fe6a7e93e250e017579ae0
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-15 08:47:41 +0200

    Special case for access denied to render that nicely in UI

--- spaces-frontend/src/utils/ajaxError.ts
+++ spaces-frontend/src/utils/ajaxError.ts
@@ -1,4 +1,13 @@
 import { AjaxError } from 'rxjs/ajax'
+import { t } from '../i18n'
 
-export const ajaxErrorToString = (e: AjaxError): string =>
-  e.response?.errors?.join('; ') || e.message || 'unknown server error'
+export const ajaxErrorToString = (e: AjaxError): string => {
+  if (e.xhr?.status === 401 || e.xhr?.status === 403) {
+    return t('general.accessDenied', 'toegang geweigerd')
+  }
+  return (
+    e.response?.errors?.join('; ') ||
+    e.message ||
+    t('general.unknownError', 'onbekende fout')
+  )
+}
