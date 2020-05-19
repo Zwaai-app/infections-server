e99412bc59083093227bde776b2bc870ed3e2700
Stefan van den Oord <soord@mac.com>
2020-05-19 17:11:55 +0200

    Properly clears status and navigates back to list after new/edit space

--- spaces-frontend/src/Spaces/AddSpace.tsx
+++ spaces-frontend/src/Spaces/AddSpace.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { t } from '../i18n'
-import { createSpace } from './spacesSlice'
+import { createSpace, clearNewSpace } from './spacesSlice'
 import { SpaceForm } from './SpaceForm'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../rootReducer'
+import { useHistory } from 'react-router-dom'
 
 export const AddSpace = () => {
+    const history = useHistory()
     const dispatch = useDispatch()
+    const spacesState = useSelector((state: RootState) => state.spaces)
+
+    useEffect(() => {
+        if
+            (spacesState.newSpace && spacesState.newSpace.status === 'success') {
+            dispatch(clearNewSpace())
+            history.push('/spaces')
+        }
+    })
 
     return <div id='AddSpace'>
         <h1>{t('AddSpace.header', 'Ruimte toevoegen')}</h1>
