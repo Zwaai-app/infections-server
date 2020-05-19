2cf7f9ea87913974803c850f39d5e5c27d6ee914
Stefan van den Oord <soord@mac.com>
2020-05-19 16:41:29 +0200

    Moves new space epic to separate file and checks ajax request

--- spaces-frontend/src/Spaces/epics/newSpace.test.ts
+++ spaces-frontend/src/Spaces/epics/newSpace.test.ts
@@ -1,24 +1,36 @@
-import { ActionsObservable } from 'redux-observable'
+import { storeNewSpaceEpic, storeAjaxOptions } from './newSpace'
+import * as O from 'fp-ts/lib/Option'
 import {
+  SpaceFields,
   createSpace,
   storeNewSpaceStarted,
   storeNewSpaceSucceeded,
-  storeNewSpaceFailed,
   loadSpaces,
-  SpaceFields
-} from './spacesSlice'
-import * as O from 'fp-ts/lib/Option'
-import { initialStateObservable } from '../testUtils/stateObservable'
-import { storeNewSpaceEpic } from './spaceEpic'
+  storeNewSpaceFailed
+} from '../spacesSlice'
+import { ActionsObservable } from 'redux-observable'
+import { initialStateObservable } from '../../testUtils/stateObservable'
 import { of, throwError } from 'rxjs'
-import { MockAjaxError } from '../testUtils/MockAjaxError'
 import { toArray } from 'rxjs/operators'
+import { MockAjaxError } from '../../testUtils/MockAjaxError'
 
 const spaceData: SpaceFields = {
   name: 'foo',
   description: 'bar',
   autoCheckout: O.some(1800)
 }
+
+it('creates the right ajax options', () => {
+  const createAction = createSpace(spaceData)
+  const options = storeAjaxOptions(createAction)
+  expect(options.method).toEqual('POST')
+  expect(options.url).toMatch(/\/api\/v1\/space$/)
+  expect(options.body.name).toEqual(spaceData.name)
+  expect(options.body.description).toEqual(spaceData.description)
+  expect(options.body.autoCheckout).toBe(1800)
+  expect(options.body.createdAt).toBeUndefined()
+  expect(options.body.modifiedAt).toBeUndefined()
+})
 
 it('can create a new space', done => {
   const action$ = ActionsObservable.of(createSpace(spaceData))
--- spaces-frontend/src/Spaces/spaceEpic.ts
+++ spaces-frontend/src/Spaces/spaceEpic.ts
@@ -1,12 +1,4 @@
-import * as A from './spacesSlice'
-import { ActionType } from 'typesafe-actions'
-import { Epic, ofType } from 'redux-observable'
-import { RootState } from '../rootReducer'
-import { Observable, of } from 'rxjs'
-import { ajax, AjaxResponse, AjaxError } from 'rxjs/ajax'
-import { flatMap, map, catchError, startWith, endWith } from 'rxjs/operators'
-import { extractAjaxErrorInfo } from '../utils/ajaxError'
-import { autoCheckoutToServer } from './conversions'
+import { storeNewSpaceEpic, Actions as CreateActions } from './epics/newSpace'
 import { updateSpaceEpic, Actions as UpdateActions } from './epics/updateSpace'
 import { deleteSpaceEpic, Actions as DeleteActions } from './epics/deleteSpace'
 import {
@@ -16,55 +8,10 @@ import {
 } from './epics/loadSpaces'
 
 export type Actions =
-  | ActionType<
-      | typeof A.createSpace
-      | typeof A.storeNewSpaceStarted
-      | typeof A.storeNewSpaceSucceeded
-      | typeof A.storeNewSpaceFailed
-    >
+  | CreateActions
   | LoadActions
   | UpdateActions
   | DeleteActions
-
-type StoreNewSpaceFn = (action: A.CreateSpaceAction) => Observable<any>
-
-const storeNewSpace: StoreNewSpaceFn = action =>
-  ajax({
-    url: 'http://localhost:3000/api/v1/space',
-    method: 'POST',
-    crossDomain: true,
-    withCredentials: true,
-    responseType: 'json',
-    body: {
-      name: action.payload.name,
-      description: action.payload.description,
-      autoCheckout: autoCheckoutToServer(action.payload.autoCheckout)
-    }
-  })
-
-const storeNewSuccess = (_r: AjaxResponse) => A.storeNewSpaceSucceeded()
-const storeNewFailed = (e: AjaxError) => {
-  return of(A.storeNewSpaceFailed(extractAjaxErrorInfo(e)))
-}
-
-export const storeNewSpaceEpic: Epic<Actions, Actions, RootState> = (
-  action$,
-  _state$,
-  {
-    storeNewSpaceFn = storeNewSpace
-  }: { storeNewSpaceFn?: StoreNewSpaceFn } = {}
-) =>
-  action$.pipe(
-    ofType<Actions, A.CreateSpaceAction>(A.createSpace.type),
-    flatMap(action =>
-      storeNewSpaceFn(action).pipe(
-        map(storeNewSuccess),
-        catchError(storeNewFailed),
-        startWith(A.storeNewSpaceStarted()),
-        endWith(A.loadSpaces())
-      )
-    )
-  )
 
 export const allEpics = [
   storeNewSpaceEpic,
