906b8e8b9706ed61e90750530cde61081b454e6f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-29 09:24:23 +0200

    Improves matcher to support passing an Eq

--- spaces-frontend/src/testUtils/fp-ts-matchers.ts
+++ spaces-frontend/src/testUtils/fp-ts-matchers.ts
@@ -2,21 +2,39 @@ import * as E from 'fp-ts/lib/Either'
 import { flow } from 'fp-ts/lib/function'
 import diff from 'jest-diff'
 import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils'
+import { Eq, eqStrict } from 'fp-ts/lib/Eq'
 
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toBeLeft(value?: any): R
-      toBeRight(value?: any): R
+      toBeLeft(value?: any, eq?: Eq<any>): R
+      toBeRight(value?: any, eq?: Eq<any>): R
     }
   }
 }
 
 expect.extend({
-  toBeLeft<E, A> (received: E.Either<E, A>, expected?: E) {
+  /**
+   * This matcher allows you to expect that an `Either` is a `left`, optionally
+   * with the expected value of the `left` and an `Eq` instance that should be
+   * used for checking the value.
+   *
+   * @param received the either that is expected to be `left`
+   * @param expected optionally, the expected value of the `left`
+   * @param eq optionally, an `Eq` instance for checking the value; if absent,
+   *           `eqStrict` is used, which corresponds to checking with `===`.
+   *
+   * @see https://gcanti.github.io/fp-ts/modules/Either.ts.html
+   * @see https://gcanti.github.io/fp-ts/modules/Eq.ts.html
+   */
+  toBeLeft<E, A> (
+    received: E.Either<E, A>,
+    expected?: E,
+    eq: Eq<E> = eqStrict
+  ) {
     return {
       pass: expected
-        ? E.exists(v => v === expected)(E.swap(received))
+        ? E.elem(eq)(expected, E.swap(received))
         : E.isLeft(received),
       message: () => {
         if (!expected) {
@@ -30,11 +48,23 @@ expect.extend({
       }
     }
   },
-  toBeRight<E, A> (received: E.Either<E, A>, expected?: A) {
+  /**
+   * This matcher allows you to expect that an `Either` is a `right`, optionally
+   * with the expected value of the `right` and an `Eq` instance that should be
+   * used for checking the value.
+   *
+   * @param received the either that is expected to be `right`
+   * @param expected optionally, the expected value of the `right`
+   * @param eq optionally, an `Eq` instance for checking the value; if absent,
+   *           `eqStrict` is used, which corresponds to checking with `===`.
+   *
+   * @see https://gcanti.github.io/fp-ts/modules/Either.ts.html
+   * @see https://gcanti.github.io/fp-ts/modules/Eq.ts.html
+   */
+  toBeRight<E, A> (received: E.Either<E, A>, expected?: A, eq?: Eq<A>) {
+    let equals = eq || eqStrict
     return {
-      pass: expected
-        ? E.exists(v => v === expected)(received)
-        : E.isRight(received),
+      pass: expected ? E.elem(equals)(expected, received) : E.isRight(received),
       message: () => {
         if (!expected) {
           return 'Either expected to be right, but was left'
