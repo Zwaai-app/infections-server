136800c47aaf9ff8a2b4b6b3e7f2885eaa7d44dc
Stefan van den Oord <soord@mac.com>
2020-06-02 17:21:57 +0200

    Adds phone validation and tests

--- spaces-frontend/src/utils/validation.ts
+++ spaces-frontend/src/utils/validation.ts
@@ -1,6 +1,7 @@
 import * as O from 'fp-ts/lib/Option'
 import { curry } from 'rambda'
 import emailAddress from 'email-addresses'
+import { PhoneNumber, parsePhoneNumberFromString } from 'libphonenumber-js'
 
 export const oneCapital = O.fromPredicate((s: string) => /[A-Z]/g.test(s))
 export const oneNumber = O.fromPredicate((s: string) => /[0-9]/g.test(s))
@@ -9,7 +10,7 @@ const minLengthPred = curry(
   (minLength: number, s: string) => s.length >= minLength
 )
 const maxLengthPred = curry(
-  (maxLength: number, s: string) => s.length < maxLength
+  (maxLength: number, s: string) => s.length <= maxLength
 )
 
 export const minLength = (n: number) => O.fromPredicate(minLengthPred(n))
@@ -19,3 +20,6 @@ type Email = ReturnType<typeof emailAddress.parseOneAddress>
 
 export const parseEmail = (email: string): O.Option<Email> =>
   O.fromNullable(emailAddress.parseOneAddress(email))
+
+export const parsePhone = (phone: string): O.Option<PhoneNumber> =>
+  O.fromNullable(parsePhoneNumberFromString(phone, 'NL'))
