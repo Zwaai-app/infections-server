bc43fcf46a247eed0887b9584ab0014b0f16647d
Stefan van den Oord <soord@mac.com>
2020-06-02 17:22:41 +0200

    Consistent naming and using flow instead of compose for readability

--- spaces-frontend/src/Register/registrationValidation.ts
+++ spaces-frontend/src/Register/registrationValidation.ts
@@ -14,7 +14,6 @@ import { NonEmptyArray, getSemigroup } from 'fp-ts/lib/NonEmptyArray'
 import { RegistrationData } from './registerSlice'
 import { constant, flow } from 'fp-ts/lib/function'
 import * as O from 'fp-ts/lib/Option'
-import { compose } from 'rambda'
 import {
   oneCapital,
   minLength,
@@ -57,42 +56,42 @@ export const tConsentRequired = t(
   'U moet met de voorwaarden akkoord gaan'
 )
 
-export const emailValid = (email: string) =>
+export const emailValidator = (email: string) =>
   flow(
     parseEmail,
     O.map(constant(email)),
     fromOption(constant(tInvalidEmail))
   )(email)
 
-export const phoneValid = (phone: string): Either<string, string> =>
+export const phoneValidator = (phone: string): Either<string, string> =>
   /[0-9]{8}/.test(phone) ? right(phone) : left(tInvalidPhone)
 
-export const consentValid = (c: boolean): Either<string, boolean> =>
+export const consentValidator = (c: boolean): Either<string, boolean> =>
   c ? right(true) : left(tConsentRequired)
 
-export const equalPasswords = (
+export const equalPasswordsValidator = (
   p1: string,
   p2: string
 ): Either<string, string> => (p1 === p2 ? right(p1) : left(tPasswordsDiffer))
 
-export const minPasswordLengthValidator = compose(
-  fromOption(constant(tPasswordLength)),
-  minLength(8)
+export const minPasswordLengthValidator = flow(
+  minLength(8),
+  fromOption(constant(tPasswordLength))
 )
 
-export const oneCapitalValidator = compose(
-  fromOption(constant(tPasswordOneCapital)),
-  oneCapital
+export const oneCapitalValidator = flow(
+  oneCapital,
+  fromOption(constant(tPasswordOneCapital))
 )
 
-export const oneNumberValidator = compose(
-  fromOption(constant(tPasswordOneNumber)),
-  oneNumber
+export const oneNumberValidator = flow(
+  oneNumber,
+  fromOption(constant(tPasswordOneNumber))
 )
 
 const applicativeValidation = () => getValidation(getSemigroup<string>())
 
-export function passwordValid (
+export function passwordValidator (
   p1: string,
   p2: string
 ): Either<NonEmptyArray<string>, string> {
@@ -101,7 +100,7 @@ export function passwordValid (
       lift(minPasswordLengthValidator)(p1),
       lift(oneCapitalValidator)(p1),
       lift(oneNumberValidator)(p1),
-      lift2(equalPasswords)(p1, p2)
+      lift2(equalPasswordsValidator)(p1, p2)
     ),
     map(constant(p1))
   )
@@ -115,9 +114,9 @@ export function validateRegistrationData (
   consent: boolean
 ): Either<NonEmptyArray<string>, RegistrationData> {
   return sequenceS(applicativeValidation())({
-    email: lift(emailValid)(email),
-    phone: lift(phoneValid)(phone),
-    password: passwordValid(p1, p2),
-    consented: lift(consentValid)(consent)
+    email: lift(emailValidator)(email),
+    phone: lift(phoneValidator)(phone),
+    password: passwordValidator(p1, p2),
+    consented: lift(consentValidator)(consent)
   })
 }
