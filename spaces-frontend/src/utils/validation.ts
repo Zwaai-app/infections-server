2028345273e61cc2a70e32fac4ea9ccb85a3c587
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-04 08:20:56 +0200

    Simplifies, curry is not needed here

--- spaces-frontend/src/utils/validation.ts
+++ spaces-frontend/src/utils/validation.ts
@@ -1,17 +1,15 @@
 import * as O from 'fp-ts/lib/Option'
-import { curry } from 'rambda'
 import emailAddress from 'email-addresses'
 import { PhoneNumber, parsePhoneNumberFromString } from 'libphonenumber-js'
 
 export const oneCapital = O.fromPredicate((s: string) => /[A-Z]/g.test(s))
 export const oneNumber = O.fromPredicate((s: string) => /[0-9]/g.test(s))
 
-const minLengthPred = curry(
-  (minLength: number, s: string) => s.length >= minLength
-)
-const maxLengthPred = curry(
-  (maxLength: number, s: string) => s.length <= maxLength
-)
+const minLengthPred = (minLength: number) => (s: string) =>
+  s.length >= minLength
+
+const maxLengthPred = (maxLength: number) => (s: string) =>
+  s.length <= maxLength
 
 export const minLength = (n: number) => O.fromPredicate(minLengthPred(n))
 export const maxLength = (n: number) => O.fromPredicate(maxLengthPred(n))
