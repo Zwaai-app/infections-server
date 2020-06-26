700babc87f0583b7deeff4863eb35370129292bd
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-26 08:46:52 +0200

    Updates preview to generate proper url in QR code

--- spaces-frontend/src/Preview/Preview.tsx
+++ spaces-frontend/src/Preview/Preview.tsx
@@ -12,57 +12,74 @@ import QRCode from 'qrcode'
 import logo from '../Home/logo.png'
 import { Icon, Button } from 'semantic-ui-react'
 import { logError } from '../utils'
+import { bytesToHex, createRandom } from './Random'
+import { autoCheckoutToNumber } from '../Spaces/conversions'
 
 export const Preview = (props: Props) => {
-    const spaces = useSelector((state: RootState) => state.spaces.spaces)
+  const spaces = useSelector((state: RootState) => state.spaces.spaces)
 
-    return <div id='spacePreview'>
-        <Button id='printButton'
-            onClick={() => { window.print() }}
-        >{t('preview.print', 'Afdrukken')}</Button>
-        {pipe(
-            O.fromNullable(props.match.params.id),
-            O.chain(id => R.lookup(id, spaces)),
-            O.fold(SpaceNotFound, ShowPreview))}
+  return (
+    <div id='spacePreview'>
+      <Button
+        id='printButton'
+        onClick={() => {
+          window.print()
+        }}
+      >
+        {t('preview.print', 'Afdrukken')}
+      </Button>
+      {pipe(
+        O.fromNullable(props.match.params.id),
+        O.chain(id => R.lookup(id, spaces)),
+        O.fold(SpaceNotFound, ShowPreview)
+      )}
     </div>
+  )
 }
 
-interface Props { match: { params: { id: string } } }
+interface Props {
+  match: { params: { id: string } }
+}
 
 const SpaceNotFound = () => {
-    const history = useHistory()
+  const history = useHistory()
 
-    useEffect(() => {
-        history.push('/spaces')
-    })
+  useEffect(() => {
+    history.push('/spaces')
+  })
 
-    return <p>{t('previewSpace.notFound', 'De ruimte is niet gevonden.')}</p>
+  return <p>{t('previewSpace.notFound', 'De ruimte is niet gevonden.')}</p>
 }
 
 const ShowPreview = (space: Space) => {
-    const profile = useSelector((state: RootState) => state.profile.data)!
+  const profile = useSelector((state: RootState) => state.profile.data)!
 
-    useEffect(() => {
-        async function generateTheCode() {
-            return QRCode.toCanvas(
-                document.getElementById('qr'),
-                'https://zwaai.app/checkin',
-                { scale: 10 })
-        }
+  useEffect(() => {
+    async function generateTheCode () {
+      const randomString = bytesToHex(createRandom())
+      const autoCheckout = autoCheckoutToNumber(space.autoCheckout)
+      const url = `zwaai-app://?random=${randomString}&type=room&autoCheckout=${autoCheckout}`
+      console.debug(url)
+      return QRCode.toCanvas(document.getElementById('qr'), url, { scale: 10 })
+    }
 
-        document.body.classList.add('preview')
+    document.body.classList.add('preview')
 
-        generateTheCode().catch(logError('Failed to generate QR code'))
-    }, [])
+    generateTheCode().catch(logError('Failed to generate QR code'))
+  }, [space])
 
-    return <>
-        <img id='logo'
-            src={profile.logo}
-            alt={t('showPreview.logoAlt', `${profile.organizationName} logo`)} />
-        <canvas id='qr' />
-        <div>
-            <h1>{space.name}</h1>
-            <p className='description'>{space.description}</p>
+  return (
+    <>
+      <img
+        id='logo'
+        src={profile.logo}
+        alt={t('showPreview.logoAlt', `${profile.organizationName} logo`)}
+      />
+      <canvas id='qr' />
+      <div>
+        <h1>{space.name}</h1>
+        <p className='description'>{space.description}</p>
+      </div>
       <div className='bottom'>
         <div className='contact'>
           {t(
@@ -82,4 +99,5 @@ const ShowPreview = (space: Space) => {
         </div>
       </div>
     </>
+  )
 }
