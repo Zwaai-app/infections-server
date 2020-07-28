72ec726ab06e428c694de2eb45967e90861733c7
Stefan van den Oord <soord@mac.com>
2020-07-28 17:00:38 +0200

    Initializes crypto before starting the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import errorHandler from 'errorhandler'
 
 import app from './app'
+import { initialize } from './controllers/spaceCheckin'
 
 /**
  * Error Handler. Provides full stack - remove for production
@@ -10,13 +11,21 @@ app.use(errorHandler())
 /**
  * Start Express server.
  */
-const server = app.listen(app.get('port'), () => {
-  console.log(
-    '  App is running at http://localhost:%d in %s mode',
-    app.get('port'),
-    app.get('env')
-  )
-  console.log('  Press CTRL-C to stop\n')
-})
+let server
+
+initialize(app)
+  .then(() => {
+    server = app.listen(app.get('port'), () => {
+      console.log(
+        '  App is running at http://localhost:%d in %s mode',
+        app.get('port'),
+        app.get('env')
+      )
+      console.log('  Press CTRL-C to stop\n')
+    })
+  })
+  .catch(error => {
+    console.error('Failed to initialize crypto', error)
+  })
 
 export default server
